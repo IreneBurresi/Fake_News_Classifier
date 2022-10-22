b5bec50830a3f2c5459d6171c10e1b24f78366ad
Irene Burresi <burresi.irene@icloud.com>
2022-10-22 21:00:25 +0200

    commit

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ selected = option_menu(
 
 
 
-model = joblib.load('./model/nb_model.pkl.pkl')
+model = joblib.load('./model/nb_model.pkl')
 
 fake_data = pd.read_csv("./data/Fake.csv")
 true_data = pd.read_csv("./data/True.csv")
